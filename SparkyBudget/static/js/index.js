55fc0ea5380e204094309a1743ec69360a65cacb
codewithcj001 <codewithcj001@gmail.com>
2025-03-27 22:29:33 -0400

    Moved navigation bar to bottom in mobile view

--- SparkyBudget/static/js/index.js
+++ SparkyBudget/static/js/index.js
@@ -461,88 +461,10 @@ function deleteBudget(subCategory) {
 
 
 
-// Function to populate the subcategory dropdown and show it
-function showSubcategoryDropdown(button) {
-    // Hide the "Re-categorize" button
-    $(button).hide();
-
-    // Show the label and dropdown
-    var row = $(button).closest('tr');
-    var dropdownLabel = row.find('.subcategoryLabel');
-    var subcategoryDropdown = row.find('#subcategorySelect');
-    var updateButton = row.find('.updateSubcategoryButton');
-
-    dropdownLabel.show();
-
-    // Show the regular dropdown
-    subcategoryDropdown.show();
-
-    // Fetch subcategories using an AJAX request
-    $.get('/getDistinctSubcategories', function (subcategories) {
-        // Clear existing options
-        subcategoryDropdown.empty();
-
-        // Populate the Select2 dropdown with fetched subcategories
-        subcategoryDropdown.select2({
-            data: subcategories,
-            width: '180px',
-            placeholder: 'Select or type to search',
-            allowClear: true,
-        });
-    });
-
-    // Show the "Update Subcategory" button
-    updateButton.show();
-}
 
 
 
-// Attach the showSubcategoryDropdown function to the "Re-categorize" button
-$('.reCategorizeButton').on('click', function () {
-    showSubcategoryDropdown(this);
-});
 
-
-var previousSubcategory = '';
-
-// Function to update the subcategory in the database
-function updateSubcategory(transactionKey, event) {
-    // Get the selected subcategory from the dropdown
-    // var updatedSubcategory = $('#subcategorySelect').val(); // Use ID selector
-    var updatedSubcategory = $('#subcategorySelect').select2('data')[0].text;
-
-
-    console.log("Inside updateSubcategory: transactionKey ", transactionKey, "updatedSubcategory :", updatedSubcategory)
-
-    // Check if a subcategory is selected
-    if (updatedSubcategory) {
-        $.ajax({
-            type: 'POST',
-            url: '/updateSubcategory',
-            data: {
-                transactionId: transactionKey,
-                updatedSubcategory: updatedSubcategory
-            },
-            success: function (response) {
-                // Handle success response
-                console.log('Subcategory updated successfully:', response);
-
-                updateBudgetSummaryChart();
-                showTransactionDetails(event, previousSubcategory);
-                setTimeout(() => {
-                    updatePieCharts(true); // Force refresh both charts
-                }, 500);
-                // You may want to update the UI or take further actions
-            },
-            error: function (error) {
-                // Handle error
-                console.error('Error updating subcategory:', error);
-            }
-        });
-    } else {
-        alert('Please select a subcategory before updating.');
-    }
-}
 // start modernizing this, baby steps with iife
 (function () {
     const hasTouchScreen = navigator && navigator.maxTouchPoints > 2;
